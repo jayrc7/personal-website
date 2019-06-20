75f43ead7e2f74855d071394f4e0cbf207be540c
Jason Cabrera <cabrerajason7@gmail.com>
2019-06-20 16:30:14 -0700

    added project item integration

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import './ProjectDisplay.css';
+import { proj } from './projects.js';
 import { Button,Container, Segment, Header, Icon, List } from 
         'semantic-ui-react';
 
 class ProjectDisplay extends React.Component{
     state = {
         currTags: {},
-        projects: []
+        projects: [],
+        currentWord: ""
     }
-
+    
     toggleTag = (event, {value}) => {
-        alert(value);
+        this.setState(prevState => ({active: !prevState.active} ) )
+        this.setState( {projects: [...this.state.projects, 1]} )
+        this.setState( {currentWord: value} )
     }
 
-    static getDerivedStateFromProps( props, state ){
+    static getDerivedStateFromProps( props, state ) {
+
     }
+
     render(){
-        const {currTags, projects} = this.state
+        const {currTags, projects, currentWord, active} = this.state
 
         return(
             <div className="display"> 
@@ -29,54 +35,62 @@ class ProjectDisplay extends React.Component{
                     <Header textAlign="left" content="Filter by tag:" 
                      size = "tiny"/>
 
-                    <Button compact size = "tiny" color = {"teal"} 
-                     className="button" value={"JQuery"} content={"JQuery"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
+                     className="button" value={"JQuery"}
+                     ref = {this.buttonRef} content={"JQuery"} 
                      onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
                      className="button" value={"React"} content={"React"} 
                      onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
                      className="button" value={"Java"} content={"Java"} 
                      onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
                      className="button" value={"Python"} content={"Python"} 
                      onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
                      className="button" value={"HTML/CSS"} content={"HTML/CSS"} 
                      onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
                      className="button" value={"Javascript"} 
                      content={"Javascript"} onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
                      className="button" value={"Hackathon"} 
                      content={"Hackathon"} onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"}
+                    <Button toggle compact size = "tiny" color = {"teal"}
                     className="button" value={"Firebase"} 
                     content={"Firebase"} onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
                      className="button" value={"Linux"} content={"Linux"} 
                      onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
                      className="button" value={"API"} content={"API"} 
                      onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
+                    <Button toggle compact size = "tiny" color = {"teal"} 
                      className="button" value={"Web"} content={"Web"} 
                      onClick = {this.toggleTag} />
 
-                    <Button compact size = "tiny" color = {"teal"} 
-                    className="button" value={"Game"} content={"Game"} 
-                    onClick = {this.toggleTag} />
+                    <Button toggle compact size = "tiny" color = {"teal"} 
+                    className="button" value={"Game"} content={"Game"}
+                    active = {active} onClick = {this.toggleTag} />
 
+                </Segment> 
+
+                <Segment inverted attached = "bottom">
+                  <div className = "as" >
+                    <ProjectItem title = {proj.projects[0].name} link = {proj.projects[0].link} />
+                  </div>
+                
                 </Segment> 
             </Container>
             </div> 
@@ -84,4 +98,30 @@ class ProjectDisplay extends React.Component{
     }
 }
 
+
+class ProjectItem extends Component {
+  render(){
+    return (
+      <Segment basic> 
+        <Segment attached = "top" clearing>
+          <div className = "sd">
+              <h1 className = "project-title"> {this.props.title}</h1> 
+              <a target = "_blank" className = "link" href = {this.props.link}> Github Repo </a>
+          </div>
+        </Segment>
+        <Segment attached>
+          <div className = "sd">
+          {proj.projects[0].tags.map((item, i) => <Button compact key = {i} color = {"teal"} content = {item} /> )}
+          </div>
+        </Segment> 
+
+        <Segment attached="bottom">
+          <div className = "sd"> 
+          {proj.projects[0].description.map((item, i) => <p className = "p-title" key = {i}> {item} </p>)}
+          </div>
+        </Segment> 
+      </Segment>
+    )
+  }
+}
 export default ProjectDisplay
---
+++ src/components/projects.js
@@ -0,0 +1,12 @@
+var proj = {
+  "projects": [
+    {
+      "name": "Personal Website",
+      "link": "https://github.com/jayrc7/personal-website",
+      "description": ["Created a personal portfolio site in React and Semantic UI, hosted on Github Pages.", "Wrote custom components in React for dynamic filtering of projects by tags."],
+      "tags": ["Javascript", "React", "API", "Web"]
+    }
+  ]
+}
+
+export { proj }
