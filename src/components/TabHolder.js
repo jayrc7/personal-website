84fe9505f86a9974b835e6bc26c47b90c9f29897
Jason Cabrera <cabrerajason7@gmail.com>
2019-08-17 22:45:31 -0700

    changed tab color

--- src/components/TabHolder.js
+++ src/components/TabHolder.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ProjectDisplay from './ProjectDisplay';
 import CourseDisplay from './CourseDisplay';
-import { Tab } from 'semantic-ui-react'
+import { Grid, Tab } from 'semantic-ui-react'
 
 const panes = [ 
-  { menuItem: 'Projects', render: () => <Tab.Pane> <ProjectDisplay /> </Tab.Pane> },
-  { menuItem: 'Courses', render: () => <Tab.Pane> <CourseDisplay /> </Tab.Pane> }
+  { menuItem: 'Projects', render: () => <Tab.Pane attached = {true}> <ProjectDisplay /> </Tab.Pane> },
+  { menuItem: 'Courses', render: () => <Tab.Pane attached = {true}> <CourseDisplay /> </Tab.Pane> }
 ]
 
-const TabHolder = () => <Tab panes = {panes} />
+
+class TabHolder extends React.Component {
+  state = { color: "grey" }
+
+  handleColorChange = e => this.setState({ color: e.target.value })
+
+  render() {
+    const { color } = this.state
+
+    return (
+      <div>
+        <Tab menu={{ color, inverted: true, attached: true, tabular: true }} panes={panes} />
+      </div>
+    )
+  }
+}
 
 export default TabHolder
