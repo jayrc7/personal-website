1e4af1bb62b10185c41fb5e5e3407869d2dfa153
Jason Cabrera <cabrerajason7@gmail.com>
2021-09-12 15:29:49 -0400

    updated path names in icons

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,9 +5,9 @@ class Links extends React.Component {
 	render() {
 		return (
 			<div>
-				<a title="Check out my projects" href='https://github.com/jayrc7' target='_blank'><img className='icon' src='../../public/githubicon.jpg' /></a>
-				<a title="Contact me via email"  href='mailto:jrcabrer@ucsd.edu'><img className='icon' src='../../public/emailicon.png' /></a>
-				<a title="Connect with me"><img  href='www.linkedin.com/in/jasonrcabrera' src='../../public/linkedinicon.png' /></a>
+				<a title='Check out my projects' href='https://github.com/jayrc7' target='_blank'><img className='icon' src='githubicon.jpg' /></a>
+				<a title='Contact me via email'  href='mailto:jrcabrer@ucsd.edu'><img className='icon' src='emailicon.png' /></a>
+				<a title='Connect with me'       href='https://www.linkedin.com/in/jasonrcabrera' target='_blank'><img  className='icon' target='_blank' src='linkedinicon.png' /></a>
 			</div>
 		);
 	}
