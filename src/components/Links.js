a2acb8700aa6362f23766cbe39a4811e2569639f
Jason Cabrera <cabrerajason7@gmail.com>
2021-09-12 15:22:35 -0400

    updated links

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,8 +6,8 @@ class Links extends React.Component {
 		return (
 			<div>
 				<a title="Check out my projects" href='https://github.com/jayrc7' target='_blank'><img className='icon' src='../../githubicon.jpg' /></a>
-				<a title="Contact me via email"><img className='icon' src='../../emailicon.png' /></a>
-				<a title="Connect with me"><img className='icon' src='../../linkedinicon.png' /></a>
+				<a title="Contact me via email"  href='mailto:jrcabrer@ucsd.edu'><img className='icon' src='../../emailicon.png' /></a>
+				<a title="Connect with me"><img  href='www.linkedin.com/in/jasonrcabrera' src='../../linkedinicon.png' /></a>
 			</div>
 		);
 	}
