fdca71df6afb6704919eec8c96f3174de82f91db
strawhatjay <cabrerajason7@gmail.com>
2021-12-22 03:02:09 -0800

    added resume link to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,12 @@ import { Link } from 'react-router-dom';
 export default function Navbar(props) {
 	return (
           <div className="Navbar">
-            <a className="NavLink"><Link to='/'>Home</Link></a>
-            <a className="NavLink"><Link to='/projects'>Projects</Link></a>
-				{/*
-            <a className="NavLink"> Experience </a>
-            <a className="NavLink"> Blog </a>
-            <a className="NavLink"> Resume </a>
-				*/}	
+              <a className="NavLink"><Link to='/'>Home</Link></a>
+              <a className="NavLink"><Link to='/projects'>Projects</Link></a>
+              <a className="NavLink" href={process.env.PUBLIC_URL + '/Cabrera, Jason Resume.pdf'} target='_blank'>Resume</a>
+              {/* <a className="NavLink"> Experience </a>
+               <a className="NavLink"> Blog </a>
+                */}
           </div>
 	);
 }
