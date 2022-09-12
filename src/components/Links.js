7079d7e506580fb2ebf6766f5e2aeacaf154cf0a
strawhatjay <cabrerajason7@gmail.com>
2022-09-11 23:04:15 -0700

    updated resume

--- src/components/Links.js
+++ src/components/Links.js
@@ -9,7 +9,7 @@ class Links extends React.Component {
 				<a title='Contact me via email' href='mailto:jrcabrer@ucsd.edu' target='_blank'><img className='icon' src='emailicon.png' /></a>
 				<a title='Connect with me' href='https://www.linkedin.com/in/jasonrcabrera' target='_blank'><img className='icon' src='linkedinicon.png' /></a>
 			</div>
-			<a className="NavLink" href={process.env.PUBLIC_URL + '/Cabrera, Jason Resume1.pdf'} target='_blank'>View Resume >></a>
+			<a className="NavLink" href={process.env.PUBLIC_URL + '/Cabrera, Jason Resume.pdf'} target='_blank'>View Resume >></a>
 			</>
 		);
 	}
