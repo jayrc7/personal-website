07cbab22e54f952613d42afb8a885b62fe9436ff
strawhatjay <cabrerajason7@gmail.com>
2022-12-28 20:31:30 -0800

    Updates

--- precache-manifest.135bc045fceb353d91d8d2c9383073bd.js
+++ precache-manifest.135bc045fceb353d91d8d2c9383073bd.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6b658e65fee4d01294ff",
+    "revision": "2e5a7fb66008fb7e497c",
     "url": "/personal-website/static/css/main.a1c34ab1.chunk.css"
   },
   {
-    "revision": "6b658e65fee4d01294ff",
-    "url": "/personal-website/static/js/main.6b658e65.chunk.js"
+    "revision": "2e5a7fb66008fb7e497c",
+    "url": "/personal-website/static/js/main.2e5a7fb6.chunk.js"
   },
   {
     "revision": "22175ee0f422651b1560",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/personal-website/static/js/2.0b680387.chunk.js"
   },
   {
-    "revision": "958fd6c183f5ad9144cd7c9c784bf580",
+    "revision": "21d1c3edd23dec1b482dd38f14f08940",
     "url": "/personal-website/index.html"
   }
 ];
