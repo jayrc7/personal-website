c8ca1fc53e5c90eca464f61a5e89319c3bba14b8
strawhatjay <cabrerajason7@gmail.com>
2023-01-05 14:38:54 -0800

    updated masters subtitle and added whitespace at end for mobile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ class Home extends React.Component {
 							<Grid.Column mobile={1} tablet={10} computer={10}>
 								<h1 className="title"> Jason Cabrera </h1>
 								<h1 className="subtitle"> Software Engineer at General Motors </h1>
-								<h1 className="subtitle"> Current Master's Student at UT Austin</h1>
+								<h1 className="subtitle"> Master's in Computer Science at UT Austin</h1>
 								<a title='Check out my projects' href='https://github.com/jayrc7' target='_blank'><img className='icon' src='githubicon.jpg' /></a>
 								<a title='Contact me via email' href='mailto:jrcabrer@ucsd.edu' target='_blank'><img className='icon' src='emailicon.png' /></a>
 								<a title='Connect with me' href='https://www.linkedin.com/in/jasonrcabrera' target='_blank'><img className='icon' src='linkedinicon.png' /></a>
@@ -28,14 +28,14 @@ class Home extends React.Component {
 					</Grid>
 				</div>
 
-				<div>
-				<h1 className="subtitle"> About Me </h1>
-				<p> Hello! I'd like to start off by thanking you for visiting my webpage! My name is Jason and I currently work as a Software Engineer at General Motors while pursuing a master's in Computer Science, with an emphasis in Machine Learning, from UT Austin.
-					As a developer, I'm passionate about full stack development, whether mobile or web based. I'm also interested in databases and of course, machine learning.
-				</p>
-				<p> 
-				Some things that I enjoy doing on my free time are playing and watching soccer, spending time with loved ones, and learning new things whether it's software engineering related or not. Feel free to look through my projects/resume to get a better idea of what I've been up to!
-				</p>
+				<div className="about-me-container">
+					<h1 className="subtitle"> About Me </h1>
+					<p> Hello! I'd like to start off by thanking you for visiting my webpage! My name is Jason and I currently work as a Software Engineer at General Motors while pursuing a master's in Computer Science, with an emphasis in Machine Learning, from UT Austin.
+						As a developer, I'm passionate about full stack development, whether mobile or web based. I'm also interested in databases and of course, machine learning.
+					</p>
+					<p> 
+					Some things that I enjoy doing on my free time are playing and watching soccer, spending time with loved ones, and learning new things whether it's software engineering related or not. Feel free to look through my projects/resume to get a better idea of what I've been up to!
+					</p>
 				</div>
 			</div>
 		);
