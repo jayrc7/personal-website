b52d1be587bdad06a932823ce2dfd69d817e7a34
strawhatjay <cabrerajason7@gmail.com>
2023-01-05 00:28:17 -0800

    added portrait to home page, updated font sizes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Projects from './pages/Projects';
 import Home from './pages/Home';
-import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
         <Router basename="/personal-website">
-            <Navbar />
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/projects" component={Projects} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import '../App.css';
 import ProjectDisplay from "../components/ProjectDisplay";
+import { Grid } from "semantic-ui-react";
 
 class Home extends React.Component {
 	render() {
 		return (
 			<div className="App">
 				<div className="header">
-					<h1 className="title"> Jason Cabrera </h1>
-					<h1 className="subtitle"> Software Engineer at General Motors </h1>
-					<h1 className="subtitle"> Current Master's Student at UT Austin</h1>
-					<div>
-						<a title='Check out my projects' href='https://github.com/jayrc7' target='_blank'><img className='icon' src='githubicon.jpg' /></a>
-						<a title='Contact me via email' href='mailto:jrcabrer@ucsd.edu' target='_blank'><img className='icon' src='emailicon.png' /></a>
-						<a title='Connect with me' href='https://www.linkedin.com/in/jasonrcabrera' target='_blank'><img className='icon' src='linkedinicon.png' /></a>
-					</div>
-					<br/>
-					<a className="NavLink" href={process.env.PUBLIC_URL + '/Cabrera, Jason Resume.pdf'} target='_blank'>View Resume >></a>
+					<Grid className="grid-height" stackable>
+						<Grid.Row>
+							<Grid.Column mobile={1} tablet={10} computer={10}>
+								<h1 className="title"> Jason Cabrera </h1>
+								<h1 className="subtitle"> Software Engineer at General Motors </h1>
+								<h1 className="subtitle"> Current Master's Student at UT Austin</h1>
+								<a title='Check out my projects' href='https://github.com/jayrc7' target='_blank'><img className='icon' src='githubicon.jpg' /></a>
+								<a title='Contact me via email' href='mailto:jrcabrer@ucsd.edu' target='_blank'><img className='icon' src='emailicon.png' /></a>
+								<a title='Connect with me' href='https://www.linkedin.com/in/jasonrcabrera' target='_blank'><img className='icon' src='linkedinicon.png' /></a>
+								<a className="NavLink" href={process.env.PUBLIC_URL + '/Cabrera, Jason Resume.pdf'} target='_blank'>View Resume >></a>
+							</Grid.Column>
+							<Grid.Column mobile={1} tablet={6} computer={6}>
+								<div>
+									<img className="me-img" src={process.env.PUBLIC_URL + '/me.png'} />
+								</div>
+							</Grid.Column>
+						</Grid.Row>
+					</Grid>
 				</div>
 
-				<h1 className="about-title"> About Me </h1>
+				<h1 className="subtitle"> About Me </h1>
 				<p> Hello! I'd like to start off by thanking you for visiting my webpage! My name is Jason and I currently work as a Software Engineer at General Motors while pursuing a master's in Computer Science, with an emphasis in Machine Learning, from UT Austin.
 					As a developer, I'm passionate about full stack development, whether mobile or web based. I'm also interested in databases and of course, machine learning.
 				</p>
@@ -27,7 +36,6 @@ class Home extends React.Component {
 			    Some things that I enjoy doing on my free time are playing and watching soccer, spending time with loved ones, and learning new things whether it's software engineering related or not. Feel free to look through my projects/resume to get a better idea of what I've been up to!
 				</p>
 
-				<ProjectDisplay />
 			</div>
 		);
 	}
